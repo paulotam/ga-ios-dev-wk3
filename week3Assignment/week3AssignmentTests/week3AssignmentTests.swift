38f35fc60d04d696ddd4f58c2a282032fd015374
Paulo Tam <Paulo.Tam@sportsbet.com.au>
2016-04-20 22:23:32 +1000

    Initial Commit

---
+++ week3Assignment/week3AssignmentTests/week3AssignmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  week3AssignmentTests.swift
+//  week3AssignmentTests
+//
+//  Created by Paulo Tam on 20/04/2016.
+//  Copyright © 2016 Paulo Tam. All rights reserved.
+//
+
+import XCTest
+@testable import week3Assignment
+
+class week3AssignmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
